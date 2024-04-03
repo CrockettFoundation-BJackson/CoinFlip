24a8cd73b3046bc2f9431e75d9b011d3632fd00e
CrockettFoundation-BJackson <162204742+CrockettFoundation-BJackson@users.noreply.github.com>
2024-04-03 13:58:57 -0400

    Fix CoinBehavior.js for multiple coins

--- CoinBehavior.js
+++ CoinBehavior.js
@@ -1,19 +1,16 @@
 const coins = document.getElementsByClassName('coin');
 
-function flipCoin() {
-  if (coins.length > 0) {
-    var coin = coins[0];
-    coin.textContent = Math.random() < 0.5 ? "H" : "T";
-    resetAnimation(coin);
-  }
+for (let coin of coins) {
+  coin.addEventListener("click", () => flipCoin(coin));
+}
+
+function flipCoin(coin) {
+  coin.textContent = Math.random() < 0.5 ? "H" : "T";
+  resetAnimation(coin);
 }
 
 function resetAnimation(coin) {
   coin.style.animation = "none";
   coin.offsetHeight;
   coin.style.animation = "flip 0.3s ease-out";
-}
-
-if (coins.length > 0) {
-  coins[0].addEventListener("click", flipCoin);
 }
